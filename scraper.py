cd48d758d7c98b960e9b89ca5dca1471a9547fda
Arthur Lozano <chillout885@gmail.com>
2022-02-05 13:53:49 -0800

    lab17

---
+++ scraper.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://en.wikipedia.org/wiki/Seattle"
+
+
+def get_citations_needed_count(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    citations = soup.find_all(class_="noprint Inline-Template Template-Fact")
+    return f"\n The number of Citations needed is: {len(citations)} \n"
+
+
+def get_citations_needed_report(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    citations = soup.find_all(class_="noprint Inline-Template Template-Fact")
+    list = 0
+    for x in citations:
+        list += 1
+        print(f"{list}. Citations needed for report: \n {x.parent.text}")
+
+
+def get_citation_headings(URL):
+    headings = []
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    anchors = soup.find_all("a")
+    heading_tags = ["h1", "h2", "h3", "h4", "h5", "h6"]
+    for anchor in anchors:
+        text = anchor.get_text()
+        if "citation needed" in text:
+            element = anchor.parent.parent.parent
+            for ps in element.previous_siblings:
+                if ps.name in heading_tags:
+                    headings.append(ps.text)
+                    break
+    return headings
+
+
+print(get_citations_needed_count(URL))
+print(get_citations_needed_report(URL))
+print(get_citation_headings(URL))
